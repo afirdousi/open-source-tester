9e97ccbf29ef697884c5c7fdb29231744a34cd8a
Firdousi <anas.firdousi@gmail.com>
2015-12-19 17:35:04 -0800

    chore(releasing setup): Added travis config, conventional commit and semantic release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,38 @@
+/**
+ * Created by afirdousi on 12/19/15.
+ */
+var expect = require('chai').expect;
+var clients = require('./index.js');
+
+describe('dummy',function(){
+
+    it('should work', function () {
+       expect(true).to.be.true;
+    });
+
+    it('shoudl return more than 1 clients', function () {
+       expect(clients.all).to.have.length.above(1);
+    });
+
+    it('should contain "lmn"',function(){
+            expect(clients.all).to.include('lmn');
+        });
+
+    it('should contain all strings',function(){
+        expect(clients.all).to.satisfy(isStringOnlyArray);
+
+    });
+
+    function isStringOnlyArray(arr){
+        return arr.every(function(item){
+            return typeof item === 'string';
+        })
+    }
+
+    it('should return a random item', function () {
+        var randomItem = clients.random();
+        expect(clients.all).to.include(randomItem);
+    })
+
+
+})
